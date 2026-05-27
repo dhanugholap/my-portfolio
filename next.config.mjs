3c9a8003ef2c5e8906d5d566ecd074e30e9a28ce
Dhananjay GHolap <dhanugholap007@gmail.com>
2026-05-28 01:20:49 +0530

    fixed cloudflare deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "standalone",
+
   reactStrictMode: false,
   allowedDevOrigins: ['192.168.0.235'],
   compress: true,
+
   images: {
     formats: ['image/webp', 'image/avif'],
     deviceSizes: [640, 750, 828, 1080, 1200],
     imageSizes: [16, 32, 64, 128, 256, 384],
     minimumCacheTTL: 60,
   },
+
   experimental: {
     optimizeCss: true,
   },
